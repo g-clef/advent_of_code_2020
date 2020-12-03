1a8c71a010908c67ed5daf02fcf75fb81d2974db
g-clef <spamblock@g-clef.net>
2020-12-03 09:42:23 -0500

    day 3, a bit more golfing

---
+++ day 3/day3.py
@@ -0,0 +1,17 @@
+import math
+field = [line.strip() for line in open("input.txt").readlines()]
+
+
+def traverse(field, side_step, height_step, pos=0):
+    hit_stuff = ""
+    for counter in range(0, len(field), height_step):
+        hit_stuff += field[counter][pos]
+        pos = (pos + side_step) % len(field[0])
+    return sum(1 for entry in hit_stuff if entry == "#")
+
+
+print(traverse(field, 3, 1))
+
+steps = ((1,1), (3,1), (5,1), (7,1), (1,2))
+
+print(math.prod(traverse(field, right, height) for right, height in steps))
