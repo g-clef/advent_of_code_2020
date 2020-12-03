53915d713e75c31913d363b97d7ed899d345adf9
g-clef <spamblock@g-clef.net>
2020-12-03 15:14:28 -0500

    Golfing day one from recommendation in slack

--- day 1/day_one.py
+++ day 1/day_one.py
@@ -1,30 +1,6 @@
-inputs = list()
+from itertools import combinations
 
-with open("input.txt") as filehandle:
-   [inputs.append(int(line.strip())) for line in filehandle]
+inputs = [int(line.strip()) for line in open("input.txt").readlines()]
 
-inputs_as_set = set(inputs)
-
-inputs.sort()
-
-for start_val in inputs:
-    compliment = 2020 - start_val
-    if compliment in inputs_as_set:
-        print(start_val * compliment)
-        break
-
-done = False
-for start_index in range(len(inputs)):
-    start_val = inputs[start_index]
-    start_compliment = 2020 - start_val
-    for end_index in range(len(inputs)-1, start_index, -1):
-        end_val = inputs[end_index]
-        if end_val > start_compliment:
-            continue
-        end_compliment = start_compliment - end_val
-        if end_compliment in inputs_as_set:
-            print(start_val * end_val * end_compliment)
-            done = True
-            break
-    if done is True:
-        break
+print([a*b for a, b in combinations(inputs, 2) if a + b == 2020][0])
+print([a*b*c for a, b, c in combinations(inputs, 3) if a + b + c == 2020][0])
